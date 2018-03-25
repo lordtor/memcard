008bc5af001ccc0c689fa10c9705253883b29bd1
Yuriy Rumyantsev <rumyanec@gmail.com>
2018-03-25 08:43:26 +0300

    update db

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-#С нашим конфигурационным файлом мы немного позаимствуем настройку конфигурации Django. У нас будет базовый
-# конфигурационный класс, на который наследуются другие классы конфигурации. Затем мы импортируем соответствующий
-# класс конфигурации по мере необходимости.
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'JlbyIrfaCnjbngjlEukjv$5Uhflecjd'
+    SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
+    #SQLALCHEMY_DATABASE_URI = os.environ['postgres://kljjqtvl:p4i93ExZ70ik9q8QsFzQvHD5MF1gnllb@baasu.db.elephantsql.com:5432/kljjqtvl']
 
 
 class ProductionConfig(Config):
